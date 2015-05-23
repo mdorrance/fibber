aa1ce6a3d2f9bdc147c066ce3b12258224748cef
Michael Dorrance <dorrancemd@gmail.com>
2015-05-23 16:36:43 -0600

    refactors into smaller methods

--- fibber.rb
+++ fibber.rb
@@ -1,72 +1,41 @@
-#The sequence begins with the numbers 0 and 1. From there, each element is the sum of the previous two elements.
-
-# Start with 0 and 1
-# 0 plus 1 is (1)
-# 1 plus 1 is (2)
-# 1 plus 2 is (3)
-# 2 plus 3 is (5)
-# 3 plus 5 is (8)
-# 5 plus 8 is (13)
-# 8 plus 13 is (21)
-# So the sequence starts 0, 1, 1, 2, 3, 5, 8, 13, 21.
-
-#TEMPLATE
-# quantity = 15
-
-# your code here
-#
-# Challenge 1: Jagged Output
-#
-# Output should come in rows of five elements. Running the program with quantity set to fifteen would output the following:
-#
-# 0 1 1 2 3
-# 5 8 13 21 34
-# 55 89 144 233 377
 require 'pry'
 
 class Fibber
-  # def fibgenerate(quantity)
-  #   #quantity is the quantity of numbers generated, so if quantity = 7 then it will generate the array [0,1,1,2,3,5,8]
-  #   #build the array of fibs until answers = quantity
-  #   number_of_fibs = []
-  #   number_of_fibs.reduce([]) do |result, quantity|
-  #     if number_of_fibs.length = 0
-  #       result = 0 << number_of_fibs
-  #       result += 1 << number_of_fibs
-  #       quantity -= quantity
-  #     elsif
-  #
-  #     end
-  #     binding.pry
-  #     result
-  #   end
-  #
-  #   quantity
-  #
-  # end
+  fibs = []
+  @fibs = fibs
 
   def long_fib_generator(quantity)
-    fibs = []
+
     quantity = quantity
     if quantity >= 2
-      fibs = [0,1]
+      @fibs = [0,1]
       (quantity - 2).times do
-        a = fibs[-1]
-        b = fibs[-2]
+        a = @fibs[-1]
+        b = @fibs[-2]
         sum = a + b
-      fibs.push(sum)
+      @fibs.push(sum)
 
-        end
-    else
-      quantity == 1
-      fibs.push(0)
-    end
-    by_fives = fibs.each_slice(5).to_a
-    by_fives.each do |arr|
-      arr.each do |inner|
-        print inner.to_s.rjust(fibs[-1].to_s.length.to_i + 1) << " "
       end
-      print "\n"
+    elsif
+      quantity == 1
+      @fibs.push(0)
+    else
+      quantity < 1
+      puts "Please enter a number greater than 1."
+      get_input
+    end
+    print_right
+    end
+    def line_width
+      @fibs[-1].to_s.length.to_i + 1
+    end
+    def print_right
+      by_fives = @fibs.each_slice(5).to_a
+      by_fives.each do |arr|
+        arr.each do |inner|
+          print inner.to_s.rjust(line_width) << " "
+        end
+        print "\n"
     end
     # print by_fives
     # print fibs
@@ -76,10 +45,21 @@ class Fibber
     # puts quantity
   end
   #fib.long_fib_generator(22)
+  def get_input
+    puts "How many numbers do you want to see in your Fibbonacci Sequence?\n"
+    input = gets.chomp
+    quantity = input.to_i
+    long_fib_generator(quantity)
+    play_again
+  end
+  def play_again
+    puts "Do you want to play again? Yes or No"
+    input = gets.chomp
+    if input.downcase == "yes"
+      get_input
+    end
+  end
 end
 
 fib = Fibber.new
-puts "How many numbers do you want to see in your Fibbonacci Sequence?\n"
-input = gets.chomp
-quantity = input.to_i
-fib.long_fib_generator(quantity)
+fib.get_input
